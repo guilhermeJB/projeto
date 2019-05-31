d06a16b24f30ad7fe020cd3ac57cdc8e3a34d9ad
aa2122 <44475587+aa2122@users.noreply.github.com>
2019-05-31 05:02:57 +0100

    Create prof-regent.component.ts

---
+++ frontend/src/app/components/prof-regente/prof-regent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prof-regente',
+  templateUrl: './prof-regente.component.html',
+  styleUrls: ['./prof-regente.component.css']
+})
+export class ProfRegenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
